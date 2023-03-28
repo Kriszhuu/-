727792fc66776ea43ebb51a07001e59b0357aaec
Kriszhuu <126672705+Kriszhuu@users.noreply.github.com>
2023-03-28 21:43:35 +0800

    Add files via upload

---
+++ exp9.asm
@@ -0,0 +1,42 @@
+assume cs:codesg
+
+data segment    ;0123456789ABCDEF
+	db	'welcome to masm!' ;ds:[bx]
+	
+	db	00000010b;green		;ds:[si]
+	db	01000010b;green characters on a red background
+	db	01110001b;blue characters on a white background
+data ends
+
+codesg segment
+start:	mov ax,data
+	mov ds,ax
+	mov bx,0
+	mov si,16	;memory address deviation in 
+	mov ax,0b800h
+	mov es,ax 	;memory address segment in later
+	mov bp,0	;memory address deviation in later
+	mov cx,3
+s0:	push bx
+	push cx
+	push bp
+	mov cx,16
+s:	mov al,[bx]
+	mov ah,[si]
+	mov es:[bp],ax
+	inc bx
+	add bp,2
+	loop s
+	add si,1
+	pop bp
+	pop cx
+	pop bx
+	add bp,160
+	loop s0
+
+
+	mov ax,4c00h
+	int 21
+	
+codesg ends
+end start
